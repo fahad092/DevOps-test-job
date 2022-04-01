82901bdda1e381c8f279de9fa7761b7494f80d9b
fahad <fahad.ibrahim@clustox.com>
2022-04-01 19:09:00 +0500

    dev-test

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ $user = 'MYSQL_USER';
 //database user password
 $pass = 'MYSQL_PASSWORD';
 
-// check the MySQL connection status
-// $conn = new mysqli($host, $user, $pass);
-// if ($conn->connect_error) {
-    // die("Connection failed: " . $conn->connect_error);
-// }
-//  else {
+//  check the MySQL connection status
+ $conn = new mysqli($host, $user, $pass);
+ if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+ }
+  else {
     echo "Connected to MySQL server successfully!";
-// }
+ }
 ?>
